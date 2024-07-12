6442fa187bbcbd4d7430b039e7e7f747f03b6bf8
Purp1e <438518244@qq.com>
2024-07-12 16:14:44 +0800

    update npm to pnpm | json to yaml | add script

--- benchmark/src/main.js
+++ benchmark/src/main.js
@@ -1,14 +1,17 @@
 /* The util to generate desired config file */
-const fs = require("fs");
-const archiver = require("archiver");
+import fs from "fs";
+import yaml from "yaml";
+import archiver from "archiver";
 
-// Setup config
-const configPath = process.argv[2] || "./config/anc-v1.json";
-const config = JSON.parse(fs.readFileSync(configPath, "utf8"));
+
+// 读取并解析 YAML 文件
+const configPath = process.argv[2] || "./config/anc-v1.yaml";
+const configFile = fs.readFileSync(configPath, "utf8");
+const config = yaml.parse(configFile);
 
 const context = {
   ...config,
-  cfgname: `${config.map.substring(0, 3)}-${config.version}`,
+  cfgname: `${config.map.substring(0, 3)}`,
   demoname: `benchmark-${config.map}-${config.version}`,
 };
 
@@ -22,22 +25,18 @@ Object.keys(context).forEach((key) => {
 });
 
 // Generate .cfg file
-const data  = fs.readFileSync("template.cfg", "utf8");
+const data = fs.readFileSync("template.cfg", "utf8");
 
 let result = data;
 Object.keys(context).forEach((key) => {
   const placeholder = `[[${key}]]`;
   const value = context[key];
-  result = result.replaceAll(placeholder, value);
+  result = result.replace(placeholder, value);
 });
 
 if (!fs.existsSync("out")) fs.mkdirSync("out");
-fs.writeFile(`out/${context.cfgname}.cfg`, result, "utf8", (err) => {
-  if (err) {
-    throw new Error("Writing file:", err);
-  }
-  console.log("[Success] Cfg file generated!");
-});
+fs.writeFileSync(`out/${context.cfgname}.cfg`, result, "utf8");
+console.log("[Success] Cfg file generated!");
 
 // Setup output file stream for zip
 const output = fs.createWriteStream(`out/${context.demoname}-rev${context.rev}.zip`);
@@ -82,6 +81,7 @@ if (!demoPath || !fs.existsSync(demoPath)) {
     ", demo will not be included in the zip file"
   );
 } else {
+  console.log("[Running] Compressing demo file:", demoPath);
   archive.file(demoPath, { name: `${context.demoname}.dem` });
 }
 
