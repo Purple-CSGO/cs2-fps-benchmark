e6f1e774cc72282426b96b76007f95ffa915fdd0
Purp1e <438518244@qq.com>
2024-07-17 16:43:52 +0800

    Revert "setup web code by nextjs"
    This reverts commit a5cbfb84d612b1edec4c025e2f800d3f1e036a24.

--- web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="px-6 py-12">
-      <div className="flex flex-col w-full max-w-4xl gap-6">
-        <code className="text-4xl font-bold">Hello, world!</code>
-      </div>
-    </div>
-  )
-}
